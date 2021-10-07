d4c9500cd2f11ae02f71552b1ffc061367cd92eb
Guillaume <guillaumesere@hotmail.fr>
2021-10-07 15:50:51 +0200

    creation modal réutilisable

--- modal.js
+++ modal.js
@@ -1,16 +1,3 @@
-
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const modalClose = document.querySelectorAll("#close");
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// close modal
-modalClose.forEach(Element=>Element.addEventListener("click", closeModal));
-
 /* function */
 function editNav() {
   var x = document.getElementById("myTopnav");
@@ -21,54 +8,92 @@ function editNav() {
   }
 }
 
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
+const modal = {
+  // Liste sur tout les liens des modals
+  modalLink: document.querySelectorAll("[data-modal]"),
+  modal: {},
+  init() {
+    // boucle sur chaque lien de modal
+    this.modalLink.forEach(link => {
+      // Récupère id de la modal
+      let mid = link.dataset.modal;
+      // stock la modal dans un tableau
+      this.modal[mid] = document.getElementById(mid);
+      if (this.modal[mid] === null) {
+        return;
+      }
+      // Evénement sur chaque modal
+      link.addEventListener("click", () => this.open(mid));
+      this.modal[mid].getElementsByClassName("close")[0].addEventListener("click", () => this.close(mid));
+    } )
+  },
+  open(mid) {
+    this.modal[mid].style.display = "block";
+  },
+  close(mid) {
+    this.modal[mid].style.display = "none";
+  }
 }
 
-// close modal
-function closeModal() {
-  modalbg.style.display = "none";
+modal.init();
+
+
+const form_inscription = {
+  error_msg: [],
+  inputs: {
+    first_name: {
+      require: true,
+      error_msg: "Le champ Prénom est requis"
+    },
+    last_name: {
+      require: true,
+      error_msg: "Le champ Nom est requis"
+    },
+    email: {
+      require: true,
+      error_msg: "Veuillez saisir une adresse email valide",
+      regex: /^[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*@[a-z0-9]+([_|\.|-]­{1}[a-z0-9]+)*[\.]{1}[a-z]{2,6}$/
+    },
+    birthdate: {
+      require: true,
+      error_msg: "Veuillez saisir une date de naissance valide",
+      regex:  /^(19|20)\d{2}[-](0?[1-9]|1[012])[-](0[1-9]|[12]\d|3[01])$/
+    }
+  },
+  onSubmit(event) {
+    event.preventDefault();
+    form_inscription.validateInputs();
+    if (form_inscription.error_msg.length > 0) {
+      form_inscription.error_msg.forEach(msg => {
+        document.getElementsByClassName("error_msg")[0].innerHTML += msg + "<br>";
+      });
+    }
+    console.log("error_message", form_inscription);
+  },
+  validateInputs() {
+    const form_inscription = document.getElementById("form_inscription");
+    this.error_msg = [];
+    document.getElementsByClassName("error_msg")[0].innerHTML = "";
+    for (const [name, input] of Object.entries(this.inputs)) {
+      form_inscription.elements[name].classList.remove("erreur");
+      if (
+          (input.require === true && form_inscription.elements[name].value.length === 0) ||
+          (input.require === true && input.regex && input.regex.exec(form_inscription.elements[name].value) === null)
+        ) {
+        this.error_msg.push(input.error_msg);
+        form_inscription.elements[name].classList.add("erreur");
+      }
+    }
+  }
 }
 
-// récupération de l'élément id="form"
-var form_inscription = document.getElementById("form");
+document.getElementById("form_inscription").addEventListener("submit", form_inscription.onSubmit);
 
-function valider (event) {
-  // code a exécuter lorsque le formulaire sera validé
-}
 
-// ajoute l’événement 
-form_inscription.addEventListener('submit', valider);
 
-// Récupération des champs du formulaire par leurs name
-var champ_prenom = form_inscription.elements["first"];
-var champ_nom = form_inscription.elements["last"];
-var champ_email = form_inscription.elements["email"];
-var champ_birthdays = form_inscription.elements["birthdate"];
-var champ_tournois = form_inscription.elements["quantity"];
-var champ_villes = form_inscription.elements["location"];
 
-// le formulaire est-il OK?
-var form_OK = true;
 
-// Au final, on empeche l'envoi du formulaire si form_OK est faux
-if(!form_OK){
-  event.preventDefault();
-}
 
-// REGEX pour la validation d'email
-var regex = /^[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*@[a-z0-9]+([_|\.|-]­{1}[a-z0-9]+)*[\.]{1}[a-z]{2,6}$/ ;
-if (regex.exec(champ_email.value) == null) {
-    form_OK = false;
-}
-
-if(champ_nom.value == ""){
-  form_OK = false;
-  champ_nom.classList.add("erreur");
-}else{
-  champ_nom.classList.remove("erreur");
-}
 
 
 
