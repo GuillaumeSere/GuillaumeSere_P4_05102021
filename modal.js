9f360b8a090329b24e97db12334962f8c57ba388
Guillaume <guillaumesere@hotmail.fr>
2021-10-09 17:33:30 +0200

    validation des champs

--- modal.js
+++ modal.js
@@ -1,96 +1,99 @@
 /* function */
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
 
 const modal = {
-  // Liste sur tout les liens des modals
-  modalLink: document.querySelectorAll("[data-modal]"),
-  modal: {},
-  init() {
-    // boucle sur chaque lien de modal
-    this.modalLink.forEach(link => {
-      // Récupère id de la modal
-      let mid = link.dataset.modal;
-      // stock la modal dans un tableau
-      this.modal[mid] = document.getElementById(mid);
-      if (this.modal[mid] === null) {
-        return;
-      }
-      // Evénement sur chaque modal
-      link.addEventListener("click", () => this.openModal(mid));
-      this.modal[mid].getElementsByClassName("close")[0].addEventListener("click", () => this.closeModal(mid));
-    } )
-  },
-  openModal(mid) {
-    this.modal[mid].style.display = "block";
-  },
-  closeModal(mid) {
-    this.modal[mid].style.display = "none";
-  }
+    // Liste sur tout les liens des modals
+    modalLink: document.querySelectorAll("[data-modal]"),
+    modal: {},
+    init() {
+        // boucle sur chaque lien de modal
+        this.modalLink.forEach(link => {
+            // Récupère id de la modal
+            let mid = link.dataset.modal;
+            // stock la modal dans un tableau
+            this.modal[mid] = document.getElementById(mid);
+            if (this.modal[mid] === null) {
+                return;
+            }
+            // Evénement sur chaque modal
+            link.addEventListener("click", () => this.open(mid));
+            this.modal[mid].getElementsByClassName("close")[0].addEventListener("click", () => this.close(mid));
+        })
+    },
+    open(mid) {
+        this.modal[mid].style.display = "block";
+    },
+    close(mid) {
+        this.modal[mid].style.display = "none";
+    }
 }
 
 modal.init();
 
-
 const form_inscription = {
-  error_msg: [],
-  inputs: {
-    first_name: {
-      require: true,
-      error_msg: "Le champ Prénom est requis"
+    error: false,
+    inputsConf: {
+        first_name: {
+            require: true,
+        },
+        last_name: {
+            require: true,
+        },
+        email: {
+            require: false,
+            regex: /^[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*@[a-z0-9]+([_|\.|-]­{1}[a-z0-9]+)*[\.]{1}[a-z]{2,6}$/
+        },
+        birthdate: {
+            require: true,
+            regex: /^(19|20)\d{2}[-](0?[1-9]|1[012])[-](0[1-9]|[12]\d|3[01])$/
+        },
+        quantity: {
+            require: true,
+        },
+        location: {
+            require: true,
+        }
     },
-    last_name: {
-      require: true,
-      error_msg: "Le champ Nom est requis"
-    },
-    email: {
-      require: true,
-      error_msg: "Veuillez saisir une adresse email valide",
-      regex: /^[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*@[a-z0-9]+([_|\.|-]­{1}[a-z0-9]+)*[\.]{1}[a-z]{2,6}$/
-    },
-    birthdate: {
-      require: true,
-      error_msg: "Veuillez saisir une date de naissance valide",
-      regex:  /^(19|20)\d{2}[-](0?[1-9]|1[012])[-](0[1-9]|[12]\d|3[01])$/
-    },
-    quantity: {
-      require: true,
-      error_msg: "Veuillez entrer un nombre"
-    },
+    onSubmit(event) {
+        event.preventDefault();
+        form_inscription.validateInputs();
+        // Submit faill
+        if (form_inscription.error === true) {
 
-  },
-  onSubmit(event) {
-    event.preventDefault();
-    form_inscription.validateInputs();
-    if (form_inscription.error_msg.length > 0) {
-      form_inscription.error_msg.forEach(msg => {
-        document.getElementsByClassName("error_msg")[0].innerHTML += msg + "<br>";
-      });
+        }
+        // Submit success
+        else {
+            modal.close("modal_inscription");
+            modal.open("modal_success_inscription");
+        }
+    },
+    validateInputs() {
+        // Retourne les éléments du formulaire
+        const form_inscription = document.getElementById("form_inscription");
+        this.error = false;
+        // Boucle sur la configuration des inputs
+        for (const [name, inputConf] of Object.entries(this.inputsConf)) {
+            // Cherche le parent du champ (form-group)
+            let formGroup = form_inscription.elements[name].length ? form_inscription.elements[name][0].parentElement : form_inscription.elements[name].parentElement;
+            // Controle des champs soumit par rapport à la configuration des champs
+            if (
+                (inputConf.require === true && form_inscription.elements[name].value.length === 0) ||
+                (inputConf.require === true && inputConf.regex && inputConf.regex.exec(form_inscription.elements[name].value) === null)
+            ) {
+                this.error = true;
+                formGroup.classList.add("error");
+            } else {
+                formGroup.classList.remove("error");
+            }
+        }
     }
-    console.log("error_message", form_inscription);
-  },
-  validateInputs() {
-    const form_inscription = document.getElementById("form_inscription");
-    this.error_msg = [];
-    document.getElementsByClassName("error_msg")[0].innerHTML = "";
-    for (const [name, input] of Object.entries(this.inputs)) {
-      form_inscription.elements[name].classList.remove("erreur");
-      if (
-          (input.require === true && form_inscription.elements[name].value.length === 0) ||
-          (input.require === true && input.regex && input.regex.exec(form_inscription.elements[name].value) === null)
-        ) {
-        this.error_msg.push(input.error_msg);
-        form_inscription.elements[name].classList.add("erreur");
-       
-      }
-    }
-  }
 }
 
 document.getElementById("form_inscription").addEventListener("submit", form_inscription.onSubmit);
