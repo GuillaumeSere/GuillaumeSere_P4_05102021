4aba8d7e70004755b6600550be0313cee23fa401
Guillaume <guillaumesere@hotmail.fr>
2021-10-15 16:14:05 +0200

    modif css modal confirmation

--- modal.js
+++ modal.js
@@ -1,11 +1,6 @@
 /* function */
-function editNav() {
-    var nav = document.getElementById("myTopnav");
-    if (nav.className === "topnav") {
-        nav.className += " responsive";
-    } else {
-        nav.className = "topnav";
-    }
+function topNavToggle() {
+   document.getElementById("myTopnav").classList.toggle("responsive");
 }
 
 const modal = {
